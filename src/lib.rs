748905c759e01a060782580a13ee4f2564c16f3b
Cameron Daniel <cam.daniel@gmail.com>
2020-12-16 07:16:51 +0100

    Fix bug where state was carried over

--- src/lib.rs
+++ src/lib.rs
@@ -14,37 +14,43 @@ lazy_static! {
     pub static ref GAUGE_PG_VERSION: GaugeVec = register_gauge_vec!(
         "patroni_postgres_version",
         "PostgreSQL version",
-        &["server", "role"]
+        &["server"]
     )
     .unwrap();
     pub static ref GAUGE_PATRONI_VERSION: GaugeVec = register_gauge_vec!(
         "patroni_version",
         "Patroni version",
-        &["server", "role", "version"]
+        &["server", "version"]
     )
     .unwrap();
     pub static ref GAUGE_RUNNING: GaugeVec = register_gauge_vec!(
         "patroni_running",
         "Is PostgreSQL running",
+        &["server"]
+    )
+    .unwrap();
+    pub static ref GAUGE_ROLE: GaugeVec = register_gauge_vec!(
+        "patroni_role",
+        "Patroni role",
         &["server", "role"]
     )
     .unwrap();
     pub static ref GAUGE_PENDING_RESTART: GaugeVec = register_gauge_vec!(
         "patroni_pending_restart",
         "Node is pending a restart",
-        &["server", "role"]
+        &["server"]
     )
     .unwrap();
     pub static ref GAUGE_TIMELINE: GaugeVec = register_gauge_vec!(
         "patroni_timeline_number",
         "Patroni timeline number",
-        &["server", "role"]
+        &["server"]
     )
     .unwrap();
     pub static ref GAUGE_REPL_SLOTS: GaugeVec = register_gauge_vec!(
         "patroni_replication_slots",
         "Postgres replication slots connected",
-        &["server", "role"]
+        &["server"]
     )
     .unwrap();
 }
@@ -107,35 +113,46 @@ pub async fn run() {
         tracing::debug!("Querying Consul");
         match consul.service(&args.service).await {
             Ok(patroni) => {
+                // Ensure we have fresh state
+                GAUGE_ROLE.reset();
+                GAUGE_PENDING_RESTART.reset();
+                GAUGE_PATRONI_VERSION.reset();
+
                 for (server, state) in &patroni {
                     let is_running = match state.is_running() {
                         true => 1.0,
                         false => 0.0,
                     };
                     GAUGE_RUNNING
-                        .with_label_values(&[server, &state.role()])
+                        .with_label_values(&[server])
                         .set(is_running);
+
+                    GAUGE_ROLE
+                        .with_label_values(&[server, &state.role()])
+                        .set(1.0);
 
                     let pending_restart = match state.pending_restart() {
                         true => 1.0,
                         false => 0.0,
                     };
                     GAUGE_PENDING_RESTART
-                        .with_label_values(&[server, &state.role()])
+                        .with_label_values(&[server])
                         .set(pending_restart);
 
                     GAUGE_PG_VERSION
-                        .with_label_values(&[server, &state.role()])
+                        .with_label_values(&[server])
                         .set(state.postgres_version() as f64);
+
                     GAUGE_PATRONI_VERSION
-                        .with_label_values(&[server, &state.role(), state.patroni_version()])
+                        .with_label_values(&[server, state.patroni_version()])
                         .set(1.0);
 
                     GAUGE_TIMELINE
-                        .with_label_values(&[server, &state.role()])
+                        .with_label_values(&[server])
                         .set(state.timeline() as f64);
+
                     GAUGE_REPL_SLOTS
-                        .with_label_values(&[server, &state.role()])
+                        .with_label_values(&[server])
                         .set(state.repl_slots() as f64);
                 }
 
